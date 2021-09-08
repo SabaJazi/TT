85647e35f7779480f3938c7dc3aedd6601d441cc
SabaJazi <saba.yousefian72@gmail.com>
2021-09-08 15:36:05 -0500

    initial

--- pre_process/frame_extr.py
+++ pre_process/frame_extr.py
@@ -8,22 +8,26 @@ def get_streams(f_movie):
                  int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT)))
     numframes = capture.get(7)
 
-    # out = cv2.VideoWriter('test_track.mp4',
-    #                       cv2.VideoWriter_fourcc(*'MP4V'),
-    #                       fps / 10,
-    #                       frameSize)
+    for i in range(numframes):
+        _, frame = capture.read()
+        # convert the RGB image to gray
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # save each frame as jpg
+        cv2.imwrite("frame%d.jpg" % i, frame)
+
     return capture
 
 ##########################################################################
 
-capture= get_streams('tumor_dual.avi')
-num_frames=int(capture.get(7))
-print(num_frames)
-for i in range(num_frames):
-  _, frame = capture.read()
-  #convert the RGB image to gray
-  frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-  #save each frame as jpg
-  cv2.imwrite("frame%d.jpg" % i, frame)
+get_streams('tumor_dual.avi')
+# capture= get_streams('tumor_dual.avi')
+# num_frames=int(capture.get(7))
+#print(num_frames)
+# for i in range(num_frames):
+#   _, frame = capture.read()
+#   #convert the RGB image to gray
+#   frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#   #save each frame as jpg
+#   cv2.imwrite("frame%d.jpg" % i, frame)
 
 
